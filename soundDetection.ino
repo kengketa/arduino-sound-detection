9766688a43fe002207a6cd167348882ad91dfde0
Sutjapong <sutjapong@conceptreach.com>
2021-08-22 13:03:48 +0700

    update: Add touvh sensor

--- soundDetection.ino
+++ soundDetection.ino
@@ -1,24 +1,33 @@
 extern volatile unsigned long timer0_millis;
 const int SOUND_SENSOR = 5;
 const int ALARM_PORT = 3;
+const int TOUCH_BUTTON = 9;
 const int DETECTION_RANGE = 10 * 1000; // msseconds
 const int DETECTION_DELAY = 0.8 * 1000; // msseconds
 const int DETECT = 0;
-const int COUNTER_LIMIT = 5;
+const int COUNTER_LIMIT = 3;
 
 int detectionCounter = 0;
 long startDetection = 0;
 boolean detected = false;
 boolean alam = false;
 
+
 void setup() {
   Serial.begin(9600);
   pinMode(SOUND_SENSOR, INPUT);
+  pinMode(TOUCH_BUTTON, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ALARM_PORT, OUTPUT);
 }
 
 void loop() {
+  
+    int resetPush = digitalRead(TOUCH_BUTTON);
+    if(resetPush == 1){
+      reset();
+    }
+    
     int statusSensor = digitalRead(SOUND_SENSOR);
     if (statusSensor == DETECT && alam == false){
         detected = true;
@@ -53,6 +62,8 @@ void loop() {
 void reset(){
   startDetection = 0;
   detectionCounter = 0;
+  alam = false;
+  digitalWrite(ALARM_PORT, LOW);
   Serial.println("reset");
 }
 
